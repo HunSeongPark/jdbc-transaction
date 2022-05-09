4cb4e86655860152f47857c03527a5e93173a4b9
Hunseong <phunseong@gmail.com>
2022-05-10 03:12:16 +0900

    MemberRepositoryv0 - update, delete test

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -4,8 +4,10 @@ import hello.jdbc.domain.Member;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * Created by Hunseong on 2022/05/10
@@ -25,5 +27,14 @@ class MemberRepositoryV0Test {
         Member findMember = repository.findById(member.getMemberId());
         assertThat(findMember).isEqualTo(member);
         assertThat(findMember.getMemberId()).isEqualTo(member.getMemberId());
+
+        // update - money : 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        // delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId())).isInstanceOf(NoSuchElementException.class);
     }
 }
