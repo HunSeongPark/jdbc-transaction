0ca86dca7ace55d65265f087190bd03133ed053f
Hunseong <phunseong@gmail.com>
2022-05-10 04:01:49 +0900

    HikariCP dataSource

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -30,6 +31,18 @@ public class ConnectionTest {
     void dataSourceDriverManager() throws SQLException {
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        useDataSource(dataSource);
+        Thread.sleep(1000); // 커넥션 생성 딜레이. 커넥션 풀은 실행 속도 영향 최소화를 위해 별도의 Thread에서 커넥션을 생성하므로 테스트가 종료되어버림.
     }
 
     private void useDataSource(DataSource dataSource) throws SQLException {
