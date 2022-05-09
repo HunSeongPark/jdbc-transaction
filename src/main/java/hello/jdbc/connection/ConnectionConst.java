621729da71e026472913d6c7f84478b9d3b93ddf
Hunseong <phunseong@gmail.com>
2022-05-10 02:38:18 +0900

    ConnectionConst

---
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,10 @@
+package hello.jdbc.connection;
+
+/**
+ * Created by Hunseong on 2022/05/10
+ */
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:h2:tcp://localhost/~/jdbc";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+}
