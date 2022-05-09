5c4d435b9e6c02b83def9df7e6dc7d822fd31697
Hunseong <phunseong@gmail.com>
2022-05-10 05:28:11 +0900

    MemberServiceV4 - MemberRepository interface

---
+++ src/main/java/hello/jdbc/service/MemberServiceV4.java
@@ -0,0 +1,39 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by Hunseong on 2022/05/10
+ */
+@Slf4j
+public class MemberServiceV4 {
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceV4(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) {
+        bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromMember.getMemberId(), fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toMember.getMemberId(), toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+}
