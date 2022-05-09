dd96e403b858063b4df999dc9422b94c7ccbca2e
Hunseong <phunseong@gmail.com>
2022-05-10 04:59:00 +0900

    MemberServiceV3_3 - @Transcational

---
+++ src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -0,0 +1,41 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+/**
+ * Created by Hunseong on 2022/05/10
+ */
+@Slf4j
+public class MemberServiceV3_3 {
+
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_3(MemberRepositoryV3 memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromMember.getMemberId(), fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toMember.getMemberId(), toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+}
