68d334b0bfd4e4ed30a556b958992dc0fee7a158
Hunseong <phunseong@gmail.com>
2022-05-10 03:06:05 +0900

    MemberRepositoryV0 - findById test

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Created by Hunseong on 2022/05/10
  */
@@ -18,5 +20,10 @@ class MemberRepositoryV0Test {
         // save
         Member member = new Member("memberV0", 10000);
         repository.save(member);
+
+        // findById
+        Member findMember = repository.findById(member.getMemberId());
+        assertThat(findMember).isEqualTo(member);
+        assertThat(findMember.getMemberId()).isEqualTo(member.getMemberId());
     }
 }
