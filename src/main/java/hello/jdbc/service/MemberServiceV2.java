209da54f08c993eb3565530e6f62bc30d9f8e976
Hunseong <phunseong@gmail.com>
2022-05-10 04:35:13 +0900

    MemberServiceV2 - use Transaction

---
+++ src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -0,0 +1,63 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Created by Hunseong on 2022/05/10
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV2 {
+
+    private final DataSource dataSource;
+    private final MemberRepositoryV1 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+        Connection con = dataSource.getConnection();
+        try {
+            con.setAutoCommit(false); // Transaction start
+            bizLogic(con, fromId, toId, money);
+            con.commit(); // Transaction commit
+        } catch (Exception e) {
+            con.rollback(); // Transaction rollback
+            throw new IllegalStateException(e);
+        } finally {
+            release(con);
+        }
+
+    }
+
+    private void bizLogic(Connection con, String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromMember.getMemberId(), fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toMember.getMemberId(), toMember.getMoney() + money);
+    }
+
+    private void release(Connection con) {
+        if (con != null) {
+            try {
+                con.setAutoCommit(true);
+                con.close();
+            } catch (Exception e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+}
